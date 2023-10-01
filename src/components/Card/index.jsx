766063ec375d4c0bfa823ff1c8e168e801831dd7
Svitlana Holovatenko <sv.golovatenko@gmail.com>
2023-10-01 12:09:25 -0700

    first-commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,47 @@
+import { primaryContext } from "../../context/PrimaryContext";
+import "./index.css";
+import React, { useContext } from "react";
+
+const Card = ({ productData, howMany }) => {
+  const { cart, setCart } = useContext(primaryContext);
+
+  const handleClick = () => {
+    //setCart to everything that was in cart already, PLUS this item
+    // check if this already in the cart
+    let indexInCart = cart.findIndex((itemInCart) => {
+      //return boolean
+      if (itemInCart.productId === productData.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    // indexInCart is the index OR -1
+
+    const newCart = [...cart];
+
+    // not in cart!
+
+    if (indexInCart === -1) {
+      newCart.push({ productId: productData.id, howMany: 1 });
+    } else {
+      newCart[indexInCart].howMany++;
+    }
+
+    setCart(newCart);
+  };
+
+  return (
+    <div className="card">
+      {howMany === undefined ? <></> : <button>Remove from cart</button>}
+      {howMany === undefined ? <></> : <span>{howMany}</span>}
+      <button onClick={handleClick}>Add To Cart</button>
+      <h3>{productData.title}</h3>
+      <img src={productData.image} alt={productData.title} />
+      <div>{productData.description}</div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Cart/index.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from "react";
+import { primaryContext } from "../../context/PrimaryContext";
+import Card from "../Card";
+
+const Cart = () => {
+  const { cart, products } = useContext(primaryContext);
+
+  //cartItem = {productId: 6, howMany: 3}
+
+  let arrayOfCards = cart.map((cartItem) => {
+    let correctProduct = products.find((prod) => {
+      if (prod.id === cartItem.productId) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    return (
+      <Card
+        key={correctProduct.title}
+        productData={correctProduct}
+        howMany={cartItem.howMany}
+      />
+    );
+  });
+
+  return (
+    <div>
+      {/*map through the cart, making a CARD for each productbin the cart. TELL THE CARD THIS IS CART CARD */}
+      <h1>CART</h1>
+      {arrayOfCards}
+    </div>
+  );
+};
+
+export default Cart;
